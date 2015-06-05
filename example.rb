327fbafc6bee643bf3b4be1d3f9a6a4f636ac18a
Kai Davenport <kaiyadavenport@gmail.com>
2015-06-05 13:15:49 +0100

    start on the Ruby example

--- example.rb
+++ example.rb
@@ -1,4 +1,5 @@
-require 'net/https'
+require "net/https"
+require "uri"
 
 # NOTE - to run this example, you need at least python 2.7.9
 
@@ -10,4 +11,27 @@ KEY_FILE = ENV['KEY_FILE'] || '/Users/kai/projects/flocker-api-examples/flockerd
 CERT_FILE = ENV['CERT_FILE'] || '/Users/kai/projects/flocker-api-examples/flockerdemo.crt'
 CA_FILE = ENV['CA_FILE'] || '/Users/kai/projects/flocker-api-examples/cluster.crt'
 
-STDOUT.write CONTROL_SERVICE
+def getTLSClient()
+    key_contents = File.read(KEY_FILE)
+    cert_contents = File.read(CERT_FILE)
+    ca_contents = File.read(CA_FILE)
+    pem = cert_contents + "\n" + ca_contents
+    http = Net::HTTP.new(CONTROL_SERVICE, CONTROL_PORT)
+    http.use_ssl = true
+    
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    http.cert = OpenSSL::X509::Certificate.new(pem)
+    http.key = OpenSSL::PKey::RSA.new(key_contents)
+    return http
+end
+
+def getURL()
+
+end
+
+client = getTLSClient()
+
+client.get2('/v1/version'){
+    |resp|
+    print resp.body
+}
